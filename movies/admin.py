f0453c8a829f58c9e54fa87a110b3481951adb63
angela64711 <angela.kiratzidou@gmail.com>
2026-06-25 11:08:50 +0200

    Customize admin.py for movie moderation

--- movies/admin.py
+++ movies/admin.py
@@ -1,10 +1,40 @@
 from django.contrib import admin
-from .models import Genre
-from .models import Movie
-from .models import Review
+from .models import Genre, Movie, Review
 
 # Register your models here.
 
-admin.site.register(Genre)
-admin.site.register(Movie)
-admin.site.register(Review)
+# Customises admin list views to support content moderation.
+
+
+@admin.register(Genre)
+class GenreAdmin(admin.ModelAdmin):
+    list_display = ("name",)
+    search_fields = ("name",)
+
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = (
+        "title",
+        "submitted_by",
+        "release_year",
+        "approved",
+        "created_on",
+    )
+    list_filter = ("approved", "genres", "created_on")
+    search_fields = ("title", "director", "submitted_by__username")
+    filter_horizontal = ("genres",)
+
+
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        "movie",
+        "author",
+        "rating",
+        "approved",
+        "is_submission_review",
+        "created_on",
+    )
+    list_filter = ("approved", "is_submission_review", "rating", "created_on")
+    search_fields = ("movie__title", "author__username", "review_text")
