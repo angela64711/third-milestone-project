01602ab46c8fd249eba05cd541026982c8bfd491
angela64711 <angela.kiratzidou@gmail.com>
2026-06-25 19:32:21 +0200

    Add approved movie list view

--- movies/views.py
+++ movies/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Movie
 
 # Create your views here.
 
@@ -7,3 +9,27 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "movies/home.html")
+
+
+# Browse Movies Page (Class view)
+
+
+class MovieList(generic.ListView):
+    """
+    Returns all submitted, approved movies in: model.Movie
+    and displays them in a page of eight posts.
+    **Context**
+
+    ``queryset``
+        All published instances of :model:`movie.Movie`
+    ``paginate_by``
+        Number of posts per page.
+
+    **Template:**
+
+    :template:`movies/browse.html`
+    """
+
+    queryset = Movie.objects.filter(approved=True)
+    template_name = "movies/browse.html"
+    paginate_by = 8
