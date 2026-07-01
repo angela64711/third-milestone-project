cb37dbfdfd999f71a621934aadd9d0d31ab62f40
angela64711 <angela.kiratzidou@gmail.com>
2026-07-01 14:25:44 +0200

    Configure Whitenoise for Heroku static files

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -58,6 +58,7 @@ ACCOUNT_SIGNUP_FIELDS = ["username*", "password1*", "password2*"]
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
